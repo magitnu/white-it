52f122e1e5c471e7b6a54ed76158aa8d7daacef6
obrist.max@gmail.com <obrist.max@gmail.com@a84ebfd3-3df6-95c1-93d7-bbbd542774cb>
2013-11-13 15:22:15 +0000

    Added entry bean methods

--- src/ch/hsr/intte/whiteit/beans/EntryBean.java
+++ src/ch/hsr/intte/whiteit/beans/EntryBean.java
@@ -1,5 +1,31 @@
 package ch.hsr.intte.whiteit.beans;
 
-public class EntryBean  extends BaseBean{
+import java.util.List;
 
+import ch.hsr.intte.whiteit.businesslogic.Logic;
+import ch.hsr.intte.whiteit.entities.Comment;
+import ch.hsr.intte.whiteit.entities.Entry;
+import ch.hsr.intte.whiteit.entities.Link;
+import ch.hsr.intte.whiteit.entities.User;
+
+public class EntryBean  extends BaseBean{
+	public int voteUp(Entry e) {
+		return Logic.entry().voteUp(e);
+	}
+	public int voteDown(Entry e) {
+		return Logic.entry().voteDown(e);
+	}
+	public List<Link> getAllLinks() {
+		return Logic.entry().getAllLinks();
+	}
+	public List<Comment> getCommentsByLink(Link link) {
+		return Logic.entry().getCommentsByEntry(link);
+	}
+	public Comment rateEntry(Entry ratedEntry, User user, String text) {
+		return Logic.entry().rateEntry(ratedEntry, user, text);
+	}
+
+	public Link createLink(User user, String url, String title) {
+		return Logic.entry().createLink(user, url, title);
+	}
 }
